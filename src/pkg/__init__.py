9318fc102bd486f4082a475db04f14874af1d2b8
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2017-12-25 17:49:58 -0500

    Add author, etc

--- src/pkg/__init__.py
+++ src/pkg/__init__.py
@@ -1,1 +1,8 @@
 """Package directory."""
+
+__author__ = ("DV Klopfenstein")
+__copyright__ = "Copyright (c) 2009-2017, Haibao Tang, DV Klopfenstein"
+__email__ = "dvklopfenstein@gmail.com"
+__license__ = "BSD"
+__status__ = "Development"
+__version__ = "0.7.11"
